1537035ba723efa0df27004dd6679b91d51f623f
mauldoto <maulanaly97@gmail.com>
2021-02-27 02:29:52 +0700

    add packet detail

---
+++ components/PacketDetail.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="container mx-auto">
+    <div class="packet-wrapper mx-4 my-8">
+      <div class="content-wrapper">
+        <div class="packet-header flex flex-col lg:flex-row">
+          <div class="packet-image lg:mr-16">
+            <img
+              class="object-cover w-full lg:w--packet-photo h-96"
+              src="~/assets/images/Details.png"
+              alt=""
+            />
+          </div>
+          <div
+            class="packet-introducing flex flex-col mt-4 lg:mt-0 lg:max-w-md"
+          >
+            <div class="packet-title mb-7">
+              <h1 class="font-bold text-3xl md:text-4xl">Rafting Program</h1>
+            </div>
+            <div class="packet-short-description">
+              <p class="font-medium text-base lg:text-lg">
+                In this program you will get information about In this program
+                you will get information about In this program you will get
+                information about In this program you will get information about
+                In this program you will get information about
+              </p>
+            </div>
+            <div class="packet-poins">
+              <ul class="">
+                <li class="flex items-center my-4">
+                  <div
+                    class="inline-block w-5 h-5 bg-yellow-900 rounded-full mr-4"
+                  ></div>
+                  <p class="font-semibold text-medium lg:text-lg">Testing</p>
+                </li>
+                <li class="flex items-center my-4">
+                  <div
+                    class="inline-block w-5 h-5 bg-yellow-900 rounded-full mr-4"
+                  ></div>
+                  <p class="font-semibold text-medium lg:text-lg">Testing</p>
+                </li>
+                <li class="flex items-center my-4">
+                  <div
+                    class="inline-block w-5 h-5 bg-yellow-900 rounded-full mr-4"
+                  ></div>
+                  <p class="font-semibold text-medium lg:text-lg">Testing</p>
+                </li>
+                <li class="flex items-center my-4">
+                  <div
+                    class="inline-block w-5 h-5 bg-yellow-900 rounded-full mr-4"
+                  ></div>
+                  <p class="font-semibold text-medium lg:text-lg">Testing</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="packet-content my-4 lg:my-8">
+          <div>
+            <h2>Seperti apasih program rafting dari kami ?</h2>
+            <br />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Ullamcorper pulvinar ut diam non, aliquam. Mauris diam elementum
+              amet eros, leo. Velit orci egestas vestibulum sit. Sed vel
+              ridiculus sapien vulputate amet nisi. Sed a eget pharetra mattis.
+              Ultrices lectus orci duis quam massa. Turpis suspendisse nisl
+              tincidunt magna auctor leo scelerisque purus enim.
+            </p>
+            <br />
+            <h2>Deskripsi</h2>
+            <br />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Ullamcorper pulvinar ut diam non, aliquam. Mauris diam elementum
+              amet eros, leo. Velit orci egestas vestibulum sit. Sed vel
+              ridiculus sapien vulputate amet nisi. Sed a eget pharetra mattis.
+              Ultrices lectus orci duis quam massa. Turpis suspendisse nisl
+              tincidunt magna auctor leo scelerisque purus enim.
+            </p>
+            <br />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Ullamcorper pulvinar ut diam non, aliquam. Mauris diam elementum
+              amet eros, leo. Velit orci egestas vestibulum sit. Sed vel
+              ridiculus sapien vulputate amet nisi. Sed a eget pharetra mattis.
+              Ultrices lectus orci duis quam massa. Turpis suspendisse nisl
+              tincidunt magna auctor leo scelerisque purus enim.
+            </p>
+            <br />
+            <h2>Rute Lokasi</h2>
+            <br />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Ullamcorper pulvinar ut diam non, aliquam. Mauris
+            </p>
+            <br />
+            <h2>Harga</h2>
+            <br />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Ullamcorper pulvinar ut diam non, aliquam. Mauris diam elementum
+              amet eros, leo. Velit orci egestas vestibulum sit. Sed vel
+              ridiculus sapien vulputate amet nisi. Sed a eget pharetra mattis.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
---
+++ components/Packets.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="container mx-auto">
+    <div class="packet-wrapper m-4">
+      <div
+        class="greeting-page text-center flex flex-col justify-center items-center"
+      >
+        <div class="price-title">
+          <h1 class="font-bold text-3xl md:text-5xl">Adventure</h1>
+        </div>
+        <div class="price-description w-full lg:w-1/2">
+          <h6 class="my-4 font-normal text-lg md:text-xl">
+            Captain Adventure memiliki program yang menarik dan seru, untuk
+            lebih jelasnya bisa lihat di bawah ini
+          </h6>
+        </div>
+      </div>
+      <div class="price-list my-4">
+        <div
+          class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-16"
+        >
+          <div class="price-item">
+            <div class="w-full relative">
+              <div class="w-full relative image-price">
+                <div
+                  class="absolute bg-gray-800 bg-opacity-40 w-full h-20 md:h-28 lg:h-32 top-0 rounded-t-xl flex flex-col lg:flex-row justify-center p-2 lg:p-5 items-center"
+                >
+                  <div
+                    class="category-detail mb-2 flex flex-col items-center justify-center text-white"
+                  >
+                    <h4
+                      class="font-semibold text-lg lg:text-xl mb-2 text-center"
+                    >
+                      Paket Karaoke bersama
+                    </h4>
+                    <p class="font-medium text-medium lg:text-lg">250k/pax</p>
+                  </div>
+                </div>
+                <img
+                  src="~/assets/images/Rafting.png"
+                  class="object-cover w-full h-96 lg:h--category-lg rounded-xl"
+                  alt=""
+                />
+                <div
+                  class="category-cta relative flex items-center justify-center"
+                >
+                  <NuxtLink
+                    class="absolute bottom-5"
+                    to="/price/category/packet"
+                  >
+                    <button
+                      class="px-6 py-3 md:px-10 md:py-4 bg-yellow-400 rounded-lg font-semibold text-lg"
+                    >
+                      Detail
+                    </button>
+                  </NuxtLink>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="price-item">
+            <div class="w-full relative">
+              <div class="w-full relative image-price">
+                <div
+                  class="absolute bg-gray-800 bg-opacity-40 w-full h-20 md:h-28 lg:h-32 top-0 rounded-t-xl flex flex-col lg:flex-row justify-center p-2 lg:p-5 items-center"
+                >
+                  <div
+                    class="category-detail mb-2 flex flex-col items-center justify-center text-white"
+                  >
+                    <h4
+                      class="font-semibold text-lg lg:text-xl mb-2 text-center"
+                    >
+                      Rafting
+                    </h4>
+                    <p class="font-medium text-medium lg:text-lg">250k/pax</p>
+                  </div>
+                </div>
+                <img
+                  src="~/assets/images/Hero.jpg"
+                  class="object-cover w-full h-96 lg:h--category-lg rounded-xl"
+                  alt=""
+                />
+                <div
+                  class="category-cta relative flex items-center justify-center"
+                >
+                  <NuxtLink
+                    class="absolute bottom-5"
+                    to="/price/category/packet"
+                  >
+                    <button
+                      class="px-6 py-3 md:px-10 md:py-4 bg-yellow-400 rounded-lg font-semibold text-lg"
+                    >
+                      Detail
+                    </button>
+                  </NuxtLink>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="price-item">
+            <div class="w-full relative">
+              <div class="w-full relative image-price">
+                <div
+                  class="absolute bg-gray-800 bg-opacity-40 w-full h-20 md:h-28 lg:h-32 top-0 rounded-t-xl flex flex-col lg:flex-row justify-center p-2 lg:p-5 items-center"
+                >
+                  <div
+                    class="category-detail mb-2 flex flex-col items-center justify-center text-white"
+                  >
+                    <h4
+                      class="font-semibold text-lg lg:text-xl mb-2 text-center"
+                    >
+                      Paintball
+                    </h4>
+                    <p class="font-medium text-medium lg:text-lg">250k/pax</p>
+                  </div>
+                </div>
+                <img
+                  src="~/assets/images/Hero.jpg"
+                  class="object-cover w-full h-96 lg:h--category-lg rounded-xl"
+                  alt=""
+                />
+                <div
+                  class="category-cta relative flex items-center justify-center"
+                >
+                  <NuxtLink
+                    class="absolute bottom-5"
+                    to="/price/category/packet"
+                  >
+                    <button
+                      class="px-6 py-3 md:px-10 md:py-4 bg-yellow-400 rounded-lg font-semibold text-lg"
+                    >
+                      Detail
+                    </button>
+                  </NuxtLink>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
