0838d44a8f910389d727b0080d085aaf310be9fb
mauldoto <maulanaly97@gmail.com>
2021-02-27 00:36:10 +0700

    category component

---
+++ components/Category.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container mx-auto">
+    <div class="category-wrapper m-4">
+      <div class="price-title">
+        <h1 class="font-bold text-3xl md:text-5xl">Harga Paket</h1>
+      </div>
+      <div class="price-description">
+        <h6 class="my-4 font-normal text-lg md:text-xl max-w-full md:w-7/12">
+          Captain Adventure memiliki program yang menarik dan seru, untuk lebih
+          jelasnya bisa lihat di bawah ini
+        </h6>
+      </div>
+      <div class="price-list my-4">
+        <div class="flex justify-center">
+          <div class="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-24">
+            <CategoryOdd />
+            <CategoryEven />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
