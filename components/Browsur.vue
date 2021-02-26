1e2f90f759a742c80a635303f3cd8a09b705c369
mauldoto <maulanaly97@gmail.com>
2021-02-27 00:36:03 +0700

    browsur component

---
+++ components/Browsur.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container mx-auto my-10">
+    <div
+      class="box-browsur mx-5 lg:m-0 p-8 border-yellow-900 rounded-xl border-4"
+    >
+      <div class="browsur-title">
+        <h3 class="font-bold text-2xl md:text-3xl">Browsur</h3>
+      </div>
+      <div
+        class="browsur-body flex flex-col md:flex-row justify-between items-center"
+      >
+        <div class="browsur-text max-w-prose my-4 md:mr-4">
+          <p class="font-normal text-lg md:text-xl">
+            Jangan khawatir, kami menyediakan brosur supaya kamu bisa lihat
+            informasi nya dan jangan lupa bagikan brosurnya kepada temanmu ya
+          </p>
+        </div>
+        <div class="browsur-cta">
+          <button
+            class="px-6 py-3 md:px-10 md:py-4 bg-yellow-400 rounded-lg font-semibold text-lg"
+          >
+            Download
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
