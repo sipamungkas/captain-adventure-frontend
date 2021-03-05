cb85dfee33706ae8839cb94bb029bc5084628a40
mauldoto <maulanaly97@gmail.com>
2021-03-05 23:01:49 +0700

    fetch data contact for footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,32 @@
   <div>
     <Navbar />
     <Nuxt />
-    <Footer />
+    <Footer :contacts="contact" :address="address" />
     <FloatWaBtn />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      contact: [],
+      address: [],
+    }
+  },
+  async fetch() {
+    const api = this.$config.baseAPIURL + 'v1/landing-page/contacts'
+    const res = await this.$axios.$get(api)
+
+    const { data } = res
+
+    this.contact = data.contacts.filter(
+      (item) => !['map', 'address', 'kontak'].includes(item.category)
+    )
+
+    this.address = data.contacts.filter((item) => item.category === 'address')
+  },
+}
+</script>
 
 <style></style>
