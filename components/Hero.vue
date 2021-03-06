0690facffe3b247cd938de6ed6f16a5a1d9f704c
mauldoto <maulanaly97@gmail.com>
2021-03-06 23:46:52 +0700

    config auto play hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -88,6 +88,10 @@ export default {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
         },
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
       },
     }
   },
