03745db230f0f8075c2ebf1bfd75e5a6fdd47442
mauldoto <maulanaly97@gmail.com>
2021-03-27 13:31:10 +0700

    handle space on date blog

--- pages/blogs/pages/_pages.vue
+++ pages/blogs/pages/_pages.vue
@@ -104,9 +104,9 @@ export default {
     const { posts, seo } = res.data
     posts.forEach((item) => {
       const datePost = new Date(item.date)
-      item.dateFormat = `${datePost.getDate()}-${
+      item.dateFormat = `${datePost.getDate()} - ${
         datePost.getMonth() + 1
-      }-${datePost.getFullYear()}`
+      } - ${datePost.getFullYear()}`
     })
     const firstPostPage = posts.length > 0 ? posts[0] : null
     if (posts.length > 0) posts.shift()
